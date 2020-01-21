597de2a9558ac4fddaf7802e8c8318f77b7b2347
Mark <mark@greencopper.com>
2020-01-20 19:15:19 -0500

    Initial Commit

---
+++ boostmi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  boostmi
+//
+//  Created by Mark Aboujaoude on 2020-01-20.
+//  Copyright © 2020 Mark Aboujaoude. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
